5c3af32eaa9765b5cf76957125ff2ed70409030e
awokeknowing <jamesdavidmorris@gmail.com>
2021-06-21 15:58:48 -0700

    files

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # dynamicCssVars
 dynamically create and modify css variables
+
+## example
+
+```js
+/* 
+
+<style>
+.mystuff {
+  background:var(--bcolor, blue);
+  height:200px
+}
+</style>  
+<div class="mystuff">
+
+*/
+
+
+let el = document.querySelector(".mystuff")
+elVars = new dynamicCssVars(el)
+
+elVars.setVar("bcolor", "brown")
+elVars.setVar("mSize", "35px")
+elVars.setVar("bcolor", "red")
+elVars.deleteVar("mSize")
+
+elVArs.dispose()
+
+```
+
+## api
+
+```js
+// create an object to manage the css vars, setting the element as scope
+constructor(element)
+
+// set a css var (the -- prefix is added)
+setVar(varName, varVal)
+
+// get the value you set (ie can't get arbitrary vars, only the ones you set)
+getVar(varName)
+
+// remove a css var that you added
+deleteVar(varName)
+
+// get rid of the dynamic stylesheet
+dispose()
+
+```
+
+## codepen
+https://codepen.io/AwokeKnowing/pen/wvJboJR
+
+
---
+++ dynamicCssVars.js
@@ -0,0 +1,51 @@
+class dynamicCssVars
+{
+    constructor(el, className)
+    {
+        this.id = className || "dynVars-"+~~(Math.random()*100000);
+        this.isDynamicId = !el.classList.contains(this.id)
+        this.el = el
+        this.el.classList.add(this.id)
+        this.vars = {}
+        this.style = document.createElement("style");
+        this.style.appendChild(document.createTextNode(""));//webkit fix
+        document.head.appendChild(this.style);
+    }
+  
+    setVar(varName, varVal)
+    {
+        if(varName in this.vars) {
+            this.vars[varName].val = '' + varVal
+            this.style.sheet.deleteRule(this.vars[varName].idx)
+            this.vars[varName].idx = this.makeRule(varName, varVal)
+        } else {
+            this.vars[varName] = {
+                val: '' + varVal,
+                idx: this.makeRule(varName, varVal)
+            }
+        }
+    }
+  
+    getVar(varName)
+    {
+        this.vars[varName].val
+    }
+  
+    deleteVar(varName)
+    {
+        this.style.sheet.deleteRule(this.vars[varName].idx)
+        delete this.vars[varName]
+    }
+  
+    makeRule(varName,varVal){
+        let rule = "."+this.id+"{--"+varName+":"+varVal+"}"
+        return this.style.sheet.insertRule(rule)
+    }
+  
+    dispose()
+    {
+        if(this.isDynamicId)
+            this.el.classList.remove(this.id)
+        this.style.remove()
+    }
+}
